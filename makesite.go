e75ae72048f9b186bf2fc24084f6a8fbc7e7e6fb
sprajjwal <shashwatprajjwal@gmail.com>
2021-02-16 16:35:44 -0800

    done

--- makesite.go
+++ makesite.go
@@ -3,17 +3,23 @@ package main
 import (
 	"bytes"
 	"flag"
-	"html/template"
+	"fmt"
 	"io/ioutil"
+	"text/template"
 )
 
-func main() {
-	filePtr := flag.String("flag", "defaultValue", "Pass the file to open.")
-	flag.Parse()
-	FileContents, err := ioutil.ReadFile("first-post.txt")
+func makeStaticPage(dir string, filename string) {
+	if dir != "." {
+		filename = dir + "/" + filename
+	}
+	fmt.Println(filename)
+	FileContents, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
+
+	sz := len(filename)
+	templateFile := filename[:sz-3] + "html"
 
 	var b bytes.Buffer
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
@@ -23,9 +29,32 @@ func main() {
 		panic(err)
 	}
 
-	err = ioutil.WriteFile(*filePtr, b.Bytes(), 777)
+	err = ioutil.WriteFile(templateFile, b.Bytes(), 777)
 	if err != nil {
 		panic(err)
 	}
 
+}
+
+func main() {
+	filePtr := flag.String("flag", "defaultValue", "Pass the file to open.")
+	dirPtr := flag.String("dir", "", "Pass the directory to look in")
+
+	flag.Parse() // parse the flags
+
+	if *filePtr != "defaultValue" {
+		makeStaticPage(".", *filePtr)
+	}
+
+	if *dirPtr != "" {
+		files, _ := ioutil.ReadDir(*dirPtr)
+		for _, file := range files {
+			sz := len(file.Name())
+			if sz > 4 {
+				if file.Name()[sz-4:sz] == ".txt" {
+					makeStaticPage(*dirPtr, file.Name())
+				}
+			}
+		}
+	}
 }
